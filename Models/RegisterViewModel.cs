038007f5e8105bfcfe4dcb34da31398ee0402b49
nenadpopovic3210@gmail.com <nenadpopovic3210@gmail.com>
2026-05-04 16:48:23 +0200

    Dodat RegisterViewModel za registracionu formu

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NutriGoal.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email je obavezan.")]
+        [EmailAddress(ErrorMessage = "Neispravan format email adrese.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Lozinka je obavezna.")]
+        [MinLength(6, ErrorMessage = "Lozinka mora imati najmanje 6 znakova.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Potvrda lozinke je obavezna")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Lozinka i potvrda lozinke se ne podudaraju.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
