f9709d5a880b95952617a97a267dc4206bfc5c15
nenadpopovic3210@gmail.com <nenadpopovic3210@gmail.com>
2026-05-29 11:30:57 +0200

    Dodata metoda za prihvaranje POST metode forme admina

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -54,6 +54,64 @@ namespace NutriGoal.Controllers
             return View("Forma", new ReceptFormViewModel());
         }
 
+        // @desc - Cuvanje novog recepta u bazu
+        // @route - POST: /Admin/Kreiraj
+        // @access - Private
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Kreiraj(ReceptFormViewModel model)
+        {
+            if (!IsAdmin()) return RedirectToAction("Login", "Korisnik");
+
+            if (!ModelState.IsValid)
+            {
+                PopuniViewBag();
+                return View("Forma", model);
+            }
+
+            var recept = new Recepti
+            {
+                Naziv = model.Naziv,
+                Opis = model.Opis,
+                Postupak = model.Postupak,
+                Fotografija = model.Fotografija,
+                KategorijaId = model.KategorijaId,
+                VrijemePripreme = model.VrijemePripreme,
+                Kalorije = model.Kalorije,
+                Proteini = model.Proteini,
+                UgljeniHidrati = model.UgljeniHidrati,
+                Masti = model.Masti,
+                DatumKreiranja = DateTime.Now
+            };
+
+            db.Recepti.Add(recept);
+            db.SaveChanges();
+
+            if (model.Sastojci != null)
+            {
+                foreach (var s in model.Sastojci.Where(s => s.SastojakId > 0 && s.KolicinaG > 0))
+                {
+                    db.ReceptSastojci.Add(new ReceptSastojci
+                    {
+                        ReceptId = recept.Id,
+                        SastojakId = s.SastojakId,
+                        KolicinaG = s.KolicinaG
+                    });
+                }
+            }
+
+            if (model.OdabraniCiljeviIds != null && model.OdabraniCiljeviIds.Any())
+            {
+                var ciljevi = db.Ciljevi.Where(c => model.OdabraniCiljeviIds.Contains(c.Id)).ToList();
+                foreach (var c in ciljevi)
+                    recept.Ciljevi.Add(c);
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         private void PopuniViewBag()
         {
             ViewBag.Kategorije = db.Kategorije.OrderBy(k => k.Naziv).ToList();
